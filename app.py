b9f30944ad8e0c387bb97a6917c6548f81e3e482
stess <stephanie.roulland@gmail.com>
2024-12-12 10:32:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask, request, jsonify
-
+from tensorflow.keras.models import load_model
+import tensorflow.keras.backend as K
+from keras.saving import register_keras_serializable
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from PIL import Image
+import io
+import base64
 
 # Initialiser l'application Flask
 app = Flask(__name__)
